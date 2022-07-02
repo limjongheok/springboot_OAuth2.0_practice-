e3be22e7e961192479f6aa10ded0fd97826e03a6
limjongheok <jjongbbang2@naver.com>
2022-07-02 20:11:25 +0900

    front 로그인

--- toyproject/src/ApiService.js
+++ toyproject/src/ApiService.js
@@ -1,16 +1,37 @@
+import { AlternateEmail } from "@material-ui/icons";
 import { API_BASE_URL } from "./app-config";
 
 
+export function signin(userDTO){
+  return call("/auth/signin", "POST", userDTO).then((response)=>{
+    
+
+    if(response.token){
+      localStorage.setItem("ACCESS_TOKEN",response.token);
+      window.location.href="/";
+    }
+  })
+}
+
+
+
 export function call(api, method, request) {
-  let options = {
-    headers: new Headers({
-      "Content-Type": "application/json",
-    }),
-    url: API_BASE_URL + api,
-    method: method,
-  };
+  let headers = new Headers({
+    "Content-Type" : "application/json",
+  });
+  const accessToken = localStorage.getItem("ACCESS_TOKEN");
+  if(accessToken && accessToken !== null){
+    headers.append("Authorization","Bearer"+accessToken);
+  }
+  let options={
+    headers : headers,
+    url : API_BASE_URL + api,
+    method:method,
+  }
+  
+  
   if (request) {
-    // GET method
+    // GET method가 아닐시 
     options.body = JSON.stringify(request);
   }
   return fetch(options.url, options).then((response) =>
@@ -21,5 +42,14 @@ export function call(api, method, request) {
       }
       return json;
     })
-  );
+  ).catch((error)=>{
+    console.log(error.status);
+    if(error.status === 403){
+      window.location.href = "/login";
+    }
+    return Promise.reject(error)
+  })
+  ;
 }
+// reton json;
+// request가 투르일시 null이 아닐시 즉 get 방식이 아닐시 fetch 로 json 반환 
